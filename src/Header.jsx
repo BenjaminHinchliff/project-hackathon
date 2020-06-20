aef7c19d6288c7df06ddb62283298686b2fc6e99
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-19 17:25:16 -0700

    wrapped header in appropriate tag

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,10 +3,12 @@ import { Jumbotron } from 'react-bootstrap';
 
 function Header() {
   return (
-    <Jumbotron className="mb-0">
-      <h2 className="display-2">Project Hackathon</h2>
-      <p className="lead">The Best (and Also First) Hackathon Run by Campo Students for the Community</p>
-    </Jumbotron>
+    <header>
+      <Jumbotron className="mb-0">
+        <h2 className="display-2">Project Hackathon</h2>
+        <p className="lead">The Best (and Also First) Hackathon Run by Campo Students for the Community</p>
+      </Jumbotron>
+    </header>
   );
 }
 
