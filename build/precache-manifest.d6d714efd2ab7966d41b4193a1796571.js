42ad87eab25f8fe9f37c75687be055192c93669c
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-19 19:50:41 -0700

    added production to repo

---
+++ build/precache-manifest.d6d714efd2ab7966d41b4193a1796571.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abadc0ed9a6e61ee5b253924a4abf0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d3f938482d07c9bd57f",
+    "url": "/static/css/main.2ef19eab.chunk.css"
+  },
+  {
+    "revision": "e5895f3af73c3249a774",
+    "url": "/static/js/2.35080580.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.35080580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d3f938482d07c9bd57f",
+    "url": "/static/js/main.bdb69a3e.chunk.js"
+  },
+  {
+    "revision": "1569b052861fefe0c623",
+    "url": "/static/js/runtime-main.1cbfc571.js"
+  },
+  {
+    "revision": "d3602f919a895b82fe207ac8374d4ed9",
+    "url": "/static/media/benjamin.d3602f91.jpg"
+  },
+  {
+    "revision": "55330f08893bcb2e675bb6b9f5706bc8",
+    "url": "/static/media/blank-avatar.55330f08.jpg"
+  },
+  {
+    "revision": "43994c60a0f19280c19201170c28ea13",
+    "url": "/static/media/cooper.43994c60.jpg"
+  }
+]);
