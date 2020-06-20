26cd06514ed172e585075cfcbb7c3e86ca4504a1
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-19 17:24:20 -0700

    added copyight in the footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <h6 className="display-6 text-muted text-center">&copy; 2020, Benjamin Hinchliff, All Rights Reserved</h6>
+    </footer>
+  );
+}
+
+export default Footer;
