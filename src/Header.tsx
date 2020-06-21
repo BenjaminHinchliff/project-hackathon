a7c208f34f5697811a5816839625c7eea846ad34
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-21 11:17:48 -0700

    added scss to change title size for smaller screens

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import './Header.scss';
 
 function Header() {
   return (
     <header>
       <Jumbotron className="mb-0 bg-info text-primary">
-        <h2 className="display-2">Project Hackathon</h2>
+        <h2 className="display-2" id="main-title">Project Hackathon</h2>
         <p className="lead">The Best (and Also First) Hackathon Run by Campo Students for the Community</p>
       </Jumbotron>
     </header>
